fd0a609781d5e919d54340182d4b59a5f4e3f9b8
adhizsrm <adhizsrm@gmail.com>
2025-05-20 14:08:29 +0530

    add conditional rendering of ingredients list and recipe section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -20,9 +20,20 @@ function Main() {
                 />
                 <button type="submit">Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+            {
+              (ingredients.length > 0)  && 
+                <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section>
+            }
         </main>
     );
 }
